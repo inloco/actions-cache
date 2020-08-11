6d9260db4f790214f7b62bd8390ba4b7813d9d3b
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2020-08-11 19:55:04 -0300

    Initial commit
    Signed-off-by: Pedro Tôrres <t0rr3sp3dr0@gmail.com>

---
+++ src/restore.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core';
+
+import restore from '../node_modules/cache/dist/restore';
+
+async function run(): Promise<void> {
+  const newCacheURL = core.getInput('cache-url', { required: true }).replace(/\/$/, '');
+  const oldCacheURL = Buffer.from(process.env['ACTIONS_CACHE_URL']!).toString('base64');
+  process.env['ACTIONS_CACHE_URL'] = `${newCacheURL}/${oldCacheURL}/`;
+
+  return restore();
+}
+
+run();
+
+export default run;
---
+++ src/save.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core';
+
+import save from '../node_modules/cache/dist/save';
+
+async function run(): Promise<void> {
+  const newCacheURL = core.getInput('cache-url', { required: true }).replace(/\/$/, '');
+  const oldCacheURL = Buffer.from(process.env['ACTIONS_CACHE_URL']!).toString('base64');
+  process.env['ACTIONS_CACHE_URL'] = `${newCacheURL}/${oldCacheURL}/`;
+
+  return save();
+}
+
+run();
+
+export default run;
