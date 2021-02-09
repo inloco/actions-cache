bbf756197e9dc4b1f58ea5179461927011ffed17
Paulo Lieuthier <paulo.lieuthier@inloco.com.br>
2021-02-09 16:44:47 -0300

    New cache implementation

--- src/save.ts
+++ src/save.ts
@@ -1,13 +1,42 @@
 import * as core from '@actions/core';
-
-import save from '../node_modules/cache/dist/save';
+import * as io from '@actions/io';
+import * as glob from '@actions/glob';
+import * as utils from './utils'
+import * as auth from './auth';
+import * as proc from './proc';
 
 async function run(): Promise<void> {
-  const newCacheURL = core.getInput('cache-url', { required: true }).replace(/\/$/, '');
-  const oldCacheURL = Buffer.from(process.env['ACTIONS_CACHE_URL']!).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
-  process.env['ACTIONS_CACHE_URL'] = `${newCacheURL}/${oldCacheURL}/`;
+  try {
+    core.info('Retrieving credentials...');
+    const key = core.getInput('key', { required: true });
+    const authResponse = await auth.get(key, auth.Type.Upload);
+    core.setSecret(authResponse.accessKeyId);
+    core.setSecret(authResponse.secretAccessKey);
+    core.setSecret(authResponse.sessionToken);
 
-  return save();
+    core.info('Setting up environment...');
+    process.env.AWS_ACCESS_KEY_ID = authResponse.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = authResponse.secretAccessKey;
+    process.env.AWS_SESSION_TOKEN = authResponse.sessionToken;
+    process.env.S3_PATH = authResponse.s3ObjectPath;
+
+    core.info('Generating files list...');
+    const patterns = core
+      .getInput('path', { required: true })
+      .split('\n')
+      .map(s => s.trim())
+      .filter(x => x !== '');
+    const globber = await glob.create(patterns.join('\n'));
+    const files = await globber.glob();
+    await utils.writeToFile('CACHEFILE.txt', files.join('\n'));
+
+    core.info('Saving cache...');
+    await proc.shell('tar -c -T CACHEFILE.txt | aws s3 cp - $S3_PATH');
+    await io.rmRF('CACHEFILE.txt');
+  } catch (error) {
+    core.error(`Failed with error: ${error}`);
+    core.setFailed(error);
+  }
 }
 
 run();
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+export function toBase64Url(input: string): string {
+  return Buffer.from(input)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+}
+
+export function writeToFile(path: string, content: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, content, error => {
+      if (error) reject(error);
+      resolve();
+    });
+  });
+}
